5c5207c6a224bec95e1d9320096c8bc5a27c8856
aleclivinghouse <aleclivinghouse@yahoo.com>
2018-08-27 09:40:13 -0700

    pass off

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,9 +1,27 @@
 const Item = (function () {
 
-   let Item = {};
+   const Item = {};
+   
+   const validateName = function(name){
+     if(Item.name !== name){
+       throw new TypeError('Name does not exist');
+     }
+   }
+
+   const create = function(name){
+     const item =  {
+       id: cuid(),
+       name: name,
+       checked: 'false'
+     }
+     return item;
+   }
 
   return {
-    Item
+    Item,
+    validateName,
+    create
   }
+
 
 }() );
--- scripts/index.js
+++ scripts/index.js
@@ -5,8 +5,17 @@
 $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
+  const itemNames = [ '', 'apples', 'pears' ];
+itemNames.forEach(name => {
+  try {
+    Item.validateName(name);
+    store.items.push(Item.create(name));
+  } catch(error) {
+    console.log('Cannot add item: ' + error.message);
+  }
+});
+shoppingList.render();
 });
 
 
 console.log(Item);
-console.log(foo);
